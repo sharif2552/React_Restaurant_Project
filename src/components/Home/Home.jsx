ef48b9bd7a9ed0320ccab44a5633275662999934
sharif2552 <86832372+sharif2552@users.noreply.github.com>
2023-10-17 21:11:06 +0600

    update

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,22 @@
 // Navigation.js
 import React from 'react';
 import { Link } from 'react-router-dom';
+import MediaCard from '../General/card';
 
 function Navigation() {
   return (
-    <nav>
+    <>
+   <MediaCard foodName={"Apple"} description={"This is apple"}/>
+   <br />
+   <MediaCard foodName={"Jilapi"} description={"This jilapi"} />
+        <nav>
       <ul>
        This is the homepage
       </ul>
+
     </nav>
+    </>
+
   ); 
 }
 
